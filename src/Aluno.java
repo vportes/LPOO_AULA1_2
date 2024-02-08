c72457207bd135cc6e0ca9f84a3fd6450dd44b0b
Vinícius Portes <141188979+vportes@users.noreply.github.com>
2024-02-07 21:59:18 -0300

    Add files via upload

---
+++ src/Aluno.java
@@ -0,0 +1,11 @@
+public class Aluno {
+    private String nome;
+    private int idade;
+    private String genero;
+
+    public Aluno() { };
+    public String toString(){
+        return "Nome do filme: "+ this.nome +
+                "\nGenero do filme: "+ this.genero;
+    }
+}
